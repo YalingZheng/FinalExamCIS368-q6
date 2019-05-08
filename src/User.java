ce9dd6e6903732e50c89b089a824b91dfd0ff44b
YalingZheng <yaling.zheng@gmail.com>
2019-05-08 11:12:27 -0400

    Create User.java

---
+++ src/User.java
@@ -0,0 +1,36 @@
+
+
+public class User {
+	private String name;
+	
+	private String password; 
+	
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+
+	public String toString() {
+		return "[name="+this.name+",password="+this.password + "]";
+	}
+
+}
